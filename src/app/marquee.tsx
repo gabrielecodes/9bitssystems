1a872e04417b75f6a96ccdc021e0ede8e28c85d9
Gabriele Costanza <84900111+gabrielecodes@users.noreply.github.com>
2025-09-17 11:42:45 +0000

    add icons in marquee

--- src/app/marquee.tsx
+++ src/app/marquee.tsx
@@ -1,8 +1,14 @@
 import { Marquee, MarqueeContent, MarqueeFade, MarqueeItem } from "./components/marquee";
 import GoogleCloud from "./components/icons/gcp";
 import Kubernetes from "./components/icons/kubernetes";
-
-const Icons = [GoogleCloud, Kubernetes];
+import Python from "./components/icons/python";
+import TypeScript from "./components/icons/typescripts";
+import Docker from "./components/icons/docker";
+import Postgres from "./components/icons/postgres";
+import Bigquery from "./components/icons/bigquery";
+import Terraform from "./components/icons/terraform";
+import Aws from "./components/icons/aws";
+import Gemini from "./components/icons/gemini";
 
 function CustomMarquqee() {
   return (
@@ -12,12 +18,35 @@ function CustomMarquqee() {
         <MarqueeFade side="right" />
         <MarqueeContent pauseOnHover={false} className="border border-neutral-200" autoFill={false}>
           <MarqueeItem className="h-fit w-32 m-2 p-2">
-            <GoogleCloud />
+            <GoogleCloud fill={"#00000023"} />
           </MarqueeItem>
-          {/* {new Array(10).fill(null).map((_, index) => (
-            <MarqueeItem className="h-32 w-32 bg-[url('/bg.jpg')] m-0 px-2" key={index}>
-            </MarqueeItem>
-          ))} */}
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Kubernetes fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Python fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <TypeScript fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Docker fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Postgres fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Bigquery fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Terraform fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Aws fill={"#00000023"} />
+          </MarqueeItem>
+          <MarqueeItem className="h-fit w-32 m-2 p-2">
+            <Gemini fill={"#00000023"} />
+          </MarqueeItem>
         </MarqueeContent>
       </Marquee>
     </div>
