f5a69ea30320e5921bae288f75035fdc02b77403
Gabriele Costanza <84900111+gabrielecodes@users.noreply.github.com>
2025-09-18 08:24:44 +0000

    update button

--- src/app/components/readmorebutton.tsx
+++ src/app/components/readmorebutton.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import ArrowRight from "./icons/arrowright";
 
 function ReadMoreButton() {
   return (
-    <p className="h-20 ml-2 col-start-2 col-span-6 flex items-center">
-      <Link href={"#"} className="font-semibold border-b border-transparent">
-        <span className="inline-block -skew-x-12 border border-foreground hover:bg-foreground hover:text-background transition-color duration-200 ease-in-out">
-          <span className="px-2 py-1 inline-block skew-x-12">&#8627;&ensp;Read More</span>
+    <p className="h-20 ml-2 col-start-2 col-span-6 flex items-center overflow-clip">
+      <Link href={"#"} className="w-1/2 h-full font-semibold flex items-center justify-center group-hover/container:border border-border transition-all duration-100 ease-in-out relative">
+        <span className="w-12 h-1/2 flex items-center opacity-0 group-hover/container:opacity-100 absolute -left-6 top-1/2 -translate-y-1/2 group-hover/container:left-4 transition-all duration-200 ease-in-out"><ArrowRight className="size-6" /></span>
+        <span className="opacity-0 group-hover/container:opacity-100 group-hover/container:text-foreground transition-all duration-200 ease-in-out">
+          Read More
         </span>
       </Link>
     </p>
