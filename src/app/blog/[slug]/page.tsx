d9e4266c1114149a550a08f048e9dbac520a26d8
Gabriele Costanza <84900111+gabrielecodes@users.noreply.github.com>
2025-09-30 13:11:57 +0000

    fix function

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,32 +17,35 @@ export default async function Blog({ params }: { params: Promise<{ slug: string 
   const date = dateArr.join(" ");
 
   return (
-    <article className="xl:w-2/5 px-4 mx-auto flex flex-col w-full text-foreground">
-      <div className="xl:h-40 h-10"></div>
-      <div className="mb-10 items-center xl:border-b xl:border-border">
-        <h1 className="leading-14">{post.frontmatter["title"] as string}</h1>
-        <div className="py-8 flex items-center justify-between relative">
-          <div className="flex items-center">
-            <div className="mr-2">Written by {post.frontmatter["author"] as string}</div>
-            <Link href={"https://www.linkedin.com/in/gabriele-costanza/"} target="_blank">
-              <LinkedIn />
-            </Link>
-            <Link href={"https://substack.com/@gabrielecodes"} target="_blank">
-              <Image src="/substack.png" alt="Substack logo" width={40} height={40} priority={true} />
-            </Link>
+    <>
+      <BlogPostingLd post={post} slug={slug} />
+      <article className="xl:w-2/5 px-4 mx-auto flex flex-col w-full text-foreground">
+        <div className="xl:h-40 h-10"></div>
+        <div className="mb-10 items-center xl:border-b xl:border-border">
+          <h1 className="leading-14">{post.frontmatter["title"] as string}</h1>
+          <div className="py-8 flex items-center justify-between relative">
+            <div className="flex items-center">
+              <div className="mr-2">Written by {post.frontmatter["author"] as string}</div>
+              <Link href={"https://www.linkedin.com/in/gabriele-costanza/"} target="_blank">
+                <LinkedIn />
+              </Link>
+              <Link href={"https://substack.com/@gabrielecodes"} target="_blank">
+                <Image src="/substack.png" alt="Substack logo" width={40} height={40} priority={true} />
+              </Link>
+            </div>
+            <p className="text-end">{date}</p>
           </div>
-          <p className="text-end">{date}</p>
         </div>
-      </div>
-      <h3 className="w-fit py-8 italic">{post.frontmatter["excerpt"] as string}</h3>
-      <div id="post-content" className="pt-6 pb-10 [&>h3]:mt-6 [&>h3]:mb-4 [&>p]:mb-4 *:l:ml-4">
-        {post.content}
-      </div>
-    </article>
+        <h3 className="w-fit py-8 italic">{post.frontmatter["excerpt"] as string}</h3>
+        <div id="post-content" className="pt-6 pb-10 [&>h3]:mt-6 [&>h3]:mb-4 [&>p]:mb-4 *:l:ml-4">
+          {post.content}
+        </div>
+      </article>
+    </>
   );
 }
 
-export async function generateMetadata({ params }: { params: { slug: string } }) {
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const post = (await getPostData(slug)) as Post;
 
